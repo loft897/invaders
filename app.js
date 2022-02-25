713d41b0a789aa9e1d392d218c32c60f92cc15e4
loft897 <salazar37@protonmail.com>
2022-02-25 05:46:23 +0000

    second

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+const container = document.querySelector('.grille');
+let toutesLesDivs;
+let alienInvaders = [];
 
+function creationGrilleEtAliens(){
+    const bloc = document.createElement('div');
+    let indexAttr = 0;
+
+    for(let i = 0; i < 240; i++){
+        if(indexAttr === 0){
+            const bloc = document.createElement('div');
+            bloc.setAttribute('data-left', 'true');
+            container.append(bloc);
+            indexAttr++;
+        }
+        else if(indexAttr === 19){
+            const bloc = document.createElement('div');
+            bloc.setAttribute('data-right', 'true');
+            container.append(bloc);
+            indexAttr = 0;
+        }
+        else {
+            const bloc = document.createElement('div');
+            container.append(bloc);
+        }
+    }
+};
+
+creationGrilleEtAliens();
